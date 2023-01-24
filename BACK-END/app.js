785f7aec40457b9658958aa987c6ac3ccfbb2274
OCTEST21 <kahasocihal@gmail.com>
2023-01-24 14:15:00 +0100

    creation MODEL

--- BACK-END/app.js
+++ BACK-END/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://octest:<password>@piquante.oakncol.mongodb.net/?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://octest:Drpepper1991@piquante.oakncol.mongodb.net/HotTakes?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -17,26 +17,6 @@ app.use((req, res, next) => {
     next();
   });
 
-  app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
-  });
+
 
 module.exports = app; 
